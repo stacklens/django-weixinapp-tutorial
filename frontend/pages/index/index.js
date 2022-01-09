ee7330ae1b66caa37117d851d1ea9432ce0058d1
Dusai <dusaiphoto@foxmail.com>
2022-01-09 12:04:19 +0800

    Update

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -1,4 +1,4 @@
-Page({
+Component({
   data: {
     items: [{
         id: 3,
@@ -15,28 +15,33 @@ Page({
     ]
   },
 
-  checkboxChange(e) {
-    // 页面持有的数据
-    const items = this.data.items
-    // checkbox持有的数据
-    const values = e.detail.value
-    // 将items和values进行对比
-    // 根据values的值更新页面数据(即data.items)
-    for (let i = 0, lenI = items.length; i < lenI; ++i) {
-      items[i].checked = false
-      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
-        // values[j]是String
-        // 将其转换为Int
-        if (items[i].id === parseInt(values[j])) {
-          items[i].checked = true
-          break
+  methods: {
+    checkboxChange(e) {
+      // 页面持有的数据
+      const items = this.data.items
+      // checkbox持有的数据
+      const values = e.detail.value
+      // 将items和values进行对比
+      // 根据values的值更新页面数据(即data.items)
+      for (let i = 0, lenI = items.length; i < lenI; ++i) {
+        items[i].checked = false
+        for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+          // values[j]是String
+          // 将其转换为Int
+          if (items[i].id === parseInt(values[j])) {
+            items[i].checked = true
+            break
+          }
         }
       }
+  
+      // 更新数据
+      this.setData({
+        items
+      })
+  
+      // 打印的内容会展现在调试器中
+      console.log(this.data.items)
     }
-
-    // 更新数据
-    this.setData({
-      items
-    })
-  }
+  },
 })
